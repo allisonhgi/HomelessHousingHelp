dbc9611d01918d067333c4250fab7031442e34a4
AllisonGibbons <agibbons67@gmail.com>
2022-04-12 22:30:14 -0700

    edited sizing

--- dist/HomelessHousingHelp.e31bb0bc.js
+++ dist/HomelessHousingHelp.e31bb0bc.js
@@ -39013,7 +39013,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62167" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58215" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
